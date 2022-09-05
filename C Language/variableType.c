ded37d3696156b0a3091ae801baf2dc3b05a9582
rokibulslam <rokibulslam6@gmail.com>
2022-09-05 19:45:29 +0600

    variable types

--- C Language/variableType.c
+++ C Language/variableType.c
@@ -1,17 +1,38 @@
 #include<stdio.h>
-
+/*
 int main(){
+    // Integer Type Variable
     int price = 100;
     printf("price = %d\n", price);
 
+    // Floating type
     float temparature = 30.3;
-    printf("temp = %f\n", temparature)
+    printf("temp = %f\n", temparature);
 
     double tempareture2 = 43.2;
     printf("temp2 = %lf\n", tempareture2);
 
     char charecter = '<';
     printf("charecter = %c\n", charecter);
+
+    return 0;
+}
+*/
+
+#include<stdio.h>
+int main(){
+
+    int marks_english;
+    int marks_math;
+
+    printf("Enter Marks in English: ");
+    scanf("%d", &marks_english);
+
+    printf("Enter Marks in Math: ");
+    scanf("%d", &marks_math);
+
+    int total_marks = marks_english + marks_math;
+    printf("Total Marks = %d", total_marks);
 
     return 0;
 }
