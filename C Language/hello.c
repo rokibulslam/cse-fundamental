3cf6b2cfdd03b4ee7fee0e096d07278a81aee83b
rokibulslam <rokibulslam6@gmail.com>
2022-09-05 19:14:41 +0600

    indention

---
+++ C Language/hello.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+//int main() {
+//    printf("Hello World\n");
+//    printf("I Love YOU");
+//    return 0;
+//}
+//Variable
+
+#include<stdio.h>
+
+int main() {
+//    integer type variable
+    int price;
+    int marks;
+
+//  Float type variable
+    float temparature;
+
+    price = 30;
+    printf("%d", price);
+}
---
+++ C Language/variableType.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int price = 100;
+    printf("price = %d\n", price);
+
+    float temparature = 30.3;
+    printf("temp = %f\n", temparature)
+
+    double tempareture2 = 43.2;
+    printf("temp2 = %lf\n", tempareture2);
+
+    char charecter = '<';
+    printf("charecter = %c\n", charecter);
+
+    return 0;
+}
